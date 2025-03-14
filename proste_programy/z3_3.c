5c743eba7248ee4cc673675473ac70bce17a1a76
Xaron11 <kaczmarzykdawid4@gmail.com>
2025-03-14 18:14:09 +0100

    C #4

--- proste_programy/z3_3.c
+++ proste_programy/z3_3.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int n, m;
-    double r;
 
-    printf("Podaj n:");
-    printf("Podaj m:");
+    printf("Podaj n: ");
+    scanf("%d", &n);
+    printf("Podaj m: ");
+    scanf("%d", &m);
 
+    double r = 0.0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        double p = 1.0;
+        double id = (double)i;
+        for (int j = 1; j <= m; j++)
+        {
+            double jd = (double)j;
+
+            p *= (id * id + jd * jd - 1) / (2 * id + 3 * jd + 4);
+        }
+        r += p;
+    }
+
+    printf("R = %lf\n", r);
 
     return 0;
 }
