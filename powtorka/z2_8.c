d0da6c43e096b9d1dbfc65f0ac012fc5e7290fec
Xaron11 <kaczmarzykdawid4@gmail.com>
2025-03-16 14:48:38 +0100

    C #8

--- powtorka/z2_8.c
+++ powtorka/z2_8.c
@@ -3,7 +3,7 @@
 
 #define N 4
 
-typedef enum Kod { AVG = 0, MIN = 1, MAX = 2 } Kod;
+typedef enum Kod { AVG = 0, MAX = 1, MIN = 2 } Kod;
 
 typedef struct SeriaPomiaru {
     int nr;
@@ -17,6 +17,46 @@ typedef struct WynikPomiaru {
     Kod kod;
 } WynikPomiaru;
 
+typedef WynikPomiaru (*FunkcjaPrzetwarzania)(SeriaPomiaru*);
+
+WynikPomiaru avg(SeriaPomiaru* seria) {
+    WynikPomiaru wynik;
+    wynik.nr = seria->nr;
+    wynik.kod = seria->kod;
+    wynik.wartosc = 0;
+    for (int i = 0; i < N; i++) {
+        wynik.wartosc += seria->pomiary[i];
+    }
+    wynik.wartosc /= N;
+    return wynik;
+}
+
+WynikPomiaru min(SeriaPomiaru* seria) {
+    WynikPomiaru wynik;
+    wynik.nr = seria->nr;
+    wynik.kod = seria->kod;
+    wynik.wartosc = seria->pomiary[0];
+    for (int i = 1; i < N; i++) {
+        if (seria->pomiary[i] < wynik.wartosc) {
+            wynik.wartosc = seria->pomiary[i];
+        }
+    }
+    return wynik;
+}
+
+WynikPomiaru max(SeriaPomiaru* seria) {
+    WynikPomiaru wynik;
+    wynik.nr = seria->nr;
+    wynik.kod = seria->kod;
+    wynik.wartosc = seria->pomiary[0];
+    for (int i = 1; i < N; i++) {
+        if (seria->pomiary[i] > wynik.wartosc) {
+            wynik.wartosc = seria->pomiary[i];
+        }
+    }
+    return wynik;
+}
+
 FILE* fopen_check(char* name, const char* mode) {
     FILE* file = fopen(name, mode);
     if (file == NULL) {
@@ -26,54 +66,40 @@ FILE* fopen_check(char* name, const char* mode) {
     return file;
 }
 
+void read_series(FILE* file_in, SeriaPomiaru* seria) {
+    for (int i = 0; i < N; i++) {
+        fscanf(file_in, "%lf", &seria->pomiary[i]);
+    }
+    int kod;
+    fscanf(file_in, "%d", &kod);
+    if (kod < 0 || kod > 2) {
+        printf("Nieprawidłowy kod: %d\n", kod);
+        exit(1);
+    }
+    seria->kod = kod;
+}
+
+void calculate(FILE* file_out, SeriaPomiaru* seria,
+               FunkcjaPrzetwarzania* funkcje) {
+    FunkcjaPrzetwarzania funkcja = funkcje[seria->kod];
+    WynikPomiaru wynik = funkcja(seria);
+    fprintf(file_out, "%d %g %d\n", wynik.nr, wynik.wartosc, wynik.kod);
+}
+
 int main() {
     FILE* file_in = fopen_check("Pomiary.txt", "r");
     FILE* file_out = fopen_check("Wyniki.txt", "w");
+    FunkcjaPrzetwarzania funkcje[3] = {avg, max, min};
 
     int nr;
     while (fscanf(file_in, "%d", &nr) != EOF) {
         SeriaPomiaru seria;
         seria.nr = nr;
-        for (int i = 0; i < N; i++) {
-            fscanf(file_in, "%lf", &seria.pomiary[i]);
-        }
-        int kod;
-        fscanf(file_in, "%d", &kod);
-        seria.kod = kod;
-
-        WynikPomiaru wynik;
-        wynik.nr = seria.nr;
-        wynik.kod = seria.kod;
-
-        switch (seria.kod) {
-            case AVG:
-                wynik.wartosc = 0;
-                for (int i = 0; i < N; i++) {
-                    wynik.wartosc += seria.pomiary[i];
-                }
-                wynik.wartosc /= N;
-                break;
-            case MIN:
-                wynik.wartosc = seria.pomiary[0];
-                for (int i = 1; i < N; i++) {
-                    if (seria.pomiary[i] < wynik.wartosc) {
-                        wynik.wartosc = seria.pomiary[i];
-                    }
-                }
-                break;
-            case MAX:
-                wynik.wartosc = seria.pomiary[0];
-                for (int i = 1; i < N; i++) {
-                    if (seria.pomiary[i] > wynik.wartosc) {
-                        wynik.wartosc = seria.pomiary[i];
-                    }
-                }
-                break;
-        }
-
-        fprintf(file_out, "%d %g %d\n", wynik.nr, wynik.wartosc, wynik.kod);
+        read_series(file_in, &seria);
+        calculate(file_out, &seria, funkcje);
     }
 
     fclose(file_in);
+    fclose(file_out);
     return 0;
 }
